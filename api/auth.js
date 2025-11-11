b96ab9e64b64f29cc4b333504d60e2b8adc202df
Tais <tfavre74@gmail.com>
2025-11-11 12:29:57 +0800

    Remove CMS setup to start fresh

--- api/auth.js
+++
@@ -1,7 +0,0 @@
-export default async (req, res) => {
-  const clientId = process.env.GITHUB_CLIENT_ID;
-  
-  const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,user&redirect_uri=${encodeURIComponent('https://jeanmarcfavre.vercel.app/api/callback')}`;
-  
-  res.redirect(authUrl);
-};
