ecbd770c7863da61f6e84aac62aaf4aa742e9420
Tais <tfavre74@gmail.com>
2025-11-11 11:47:20 +0800

    Add Vercel OAuth serverless functions for Decap CMS

---
+++ api/auth.js
@@ -0,0 +1,24 @@
+const simpleOauth2 = require('simple-oauth2');
+
+module.exports = async (req, res) => {
+  const oauth2 = simpleOauth2.create({
+    client: {
+      id: process.env.GITHUB_CLIENT_ID,
+      secret: process.env.GITHUB_CLIENT_SECRET,
+    },
+    auth: {
+      tokenHost: 'https://github.com',
+      tokenPath: '/login/oauth/access_token',
+      authorizePath: '/login/oauth/authorize',
+    },
+  });
+
+  const authorizationUri = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: `${process.env.VERCEL_URL || 'https://jeanmarcfavre.vercel.app'}/api/callback`,
+    scope: 'repo,user',
+    state: req.query.state || '',
+  });
+
+  res.redirect(authorizationUri);
+};
+
