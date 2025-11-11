2590563a37e25dccaaee79ff8d1850a246e16771
Tais <tfavre74@gmail.com>
2025-11-11 13:29:35 +0800

    Update build scripts for Tina CMS

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/tais/Desktop/Websites/jeanmarcfavre/tina/__generated__/.cache/1762838195054', url: 'https://content.tinajs.io/1.6/content/1b3e810a-ad61-42fe-9c1c-d2a330e7e30e/github/main', token: '9d52f8159e90f26c8e1a9eb52b76d32504bbdfd5', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
 export default client;
   
