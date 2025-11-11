86c9bf85c481be3560682662f92af8ea87ce2306
Tais <tfavre74@gmail.com>
2025-11-11 13:19:03 +0800

    Add Tina admin HTML and remove server-rendered page

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,2 +1,5 @@
-export default ()=>({})
-export const client = ()=>({})
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/Users/tais/Desktop/Websites/jeanmarcfavre/tina/__generated__/.cache/1762838195054', url: 'https://content.tinajs.io/1.6/content/1b3e810a-ad61-42fe-9c1c-d2a330e7e30e/github/main', token: '9d52f8159e90f26c8e1a9eb52b76d32504bbdfd5', queries,  });
+export default client;
+  
